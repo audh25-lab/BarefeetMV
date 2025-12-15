28a3420786d8908d3729a0a4a35e90f777b6c160
audh25-lab <audh25@gmail.com>
2025-12-16 04:53:22 +0500

    Update AmbientField.ts

--- ui-postscreen/AmbientField.ts
+++ ui-postscreen/AmbientField.ts
@@ -1,11 +1,6 @@
 export class AmbientField {
   private intensity = 0.5
 
-  update(emotion: string) {
-    if (emotion === "calm") this.intensity -= 0.05
-    if (emotion === "curious") this.intensity += 0.1
-  }
-
   state() {
     return {
       glow: this.intensity,
